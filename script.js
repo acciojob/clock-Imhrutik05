ae31cfcf168fa61818e5fc95d83363d1049174c8
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-20 20:00:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let element = document.querySelector('p');
-
-let date = new Date();
-
-let month = date.getMonth();
-let dateD = date.getDate();
-let year = date.getFullYear();
-let hours = date.getHours();
-let hoursD = hours % 12;
-let mins = date.getMinutes();
-let secs = date.getSeconds();
-let ampm = hours >= 12 ? "PM" : "AM";
-
-element.innerHTML = `
-	${month}/${dateD}/${year}, ${hoursD}:${mins}:${secs} ${ampm}
-`;
+function render(){
+	let element = document.querySelector('p');
+	let date = new Date();
+	
+	let month = date.getMonth();
+	let dateD = date.getDate();
+	let year = date.getFullYear();
+	let hours = date.getHours();
+	let hoursD = hours % 12;
+	let mins = date.getMinutes();
+	let secs = date.getSeconds();
+	let ampm = hours >= 12 ? "PM" : "AM";
+	element.innerHTML = `
+		${month}/${dateD}/${year}, ${hoursD}:${mins}:${secs} ${ampm}
+	`;
+}
+setInterval(render, 999);
