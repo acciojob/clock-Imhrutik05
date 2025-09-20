c4983befc62a48aae75bd1609c3975f627272c7d
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-20 19:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let element = document.querySelector('p');
+
+let date = new Date();
+
+let month = date.getMonth();
+let dateD = date.getDate();
+let year = date.getFullYear();
+let hours = date.getHours();
+let hoursD = hours % 12;
+let mins = date.getMinutes();
+let secs = date.getSeconds();
+let ampm = hours >= 12 ? "PM" : "AM";
+
+element.innerHTML = `
+	${month}/${dateD}/${year}, ${hoursD}:${mins}:${secs} ${ampm}
+`;
